2ec1b1cf35ceb773cc4ca7e5047d26bfd27f107c
Fumiya Funatsu <funatsu.fumiya@gmail.com>
2015-07-05 23:07:21 +0900

    Add hardest sudoku

--- test/test.js
+++ test/test.js
@@ -93,3 +93,33 @@ QUnit.test("Sudoku solve - level 3", function(assert){
   var actual = Sudoku.beautify(Sudoku.solve(q));
   assert.equal(actual,expected);
 });
+
+QUnit.test("Sudoku solve - hardest", function(assert){
+  var _ = Sudoku.empty;
+  var q = [
+      _,_,5,3,_,_,_,_,_,
+      8,_,_,_,_,_,_,2,_,
+      _,7,_,_,1,_,5,_,_,
+      4,_,_,_,_,5,3,_,_,
+      _,1,_,_,7,_,_,_,6,
+      _,_,3,2,_,_,_,8,_,
+      _,6,_,5,_,_,_,_,9,
+      _,_,4,_,_,_,_,3,_,
+      _,_,_,_,_,9,7,_,_
+    ];
+  var expected = Sudoku.beautify([
+    1,4,5,3,2,7,6,9,8,
+    8,3,9,6,5,4,1,2,7,
+    6,7,2,9,1,8,5,4,3,
+    4,9,6,1,8,5,3,7,2,
+    2,1,8,4,7,3,9,5,6,
+    7,5,3,2,9,6,4,8,1,
+    3,6,7,5,4,2,8,1,9,
+    9,8,4,7,6,1,2,3,5,
+    5,2,1,8,3,9,7,6,4
+  ]);
+
+
+  var actual = Sudoku.beautify(Sudoku.solve(q));
+  assert.equal(actual,expected);
+});
